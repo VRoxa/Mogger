105d0b9429d352c378f0f403207a0fabc10f2876
VRoxa <victor.roxa.r@gmail.com>
2022-04-04 17:48:13 +0200

    Implement Mogger assertion extensions tests

---
+++ Mogger.Tests/MoggerAssertionsTests.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Extensions.Logging;
+using Mogger.Tests.Service;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Mogger.Tests;
+
+public class MoggerAssertionsTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public MoggerAssertionsTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void OnLogging_WithService_AtInfo_Log_IsCalled()
+    {
+        // Arrange
+        var logger = Mogger<FakeService>.Create(_testOutputHelper);
+        var service = new FakeService(logger);
+
+        // Act
+        service.DoWork();
+
+        // Assert
+        logger.MustHaveLogged(LogLevel.Information);
+    }
+
+    [Fact]
+    public void OnLogging_WithService_Log_IsCalled()
+    {
+        // Arrange
+        var logger = Mogger<FakeService>.Create(_testOutputHelper);
+        var service = new FakeService(logger);
+
+        // Act
+        service.DoWork();
+
+        // Assert
+        logger.MustHaveLogged();
+    }
+
+    [Fact]
+    public void OnLogging_WithService_MatchingPredicate_IsSatisfied()
+    {
+        // Arrange
+        var logger = Mogger<FakeService>.Create(_testOutputHelper);
+        var service = new FakeService(logger);
+
+        // Act
+        service.DoWork();
+
+        // Assert
+        logger.MustHaveLogged(args =>
+        {
+            var message = args.Get<object>("state");
+            return message?.ToString() == "Doing some work...";
+        });
+    }
+
+    [Fact]
+    public void OnLogging_WithService_MismatchingPredicate_IsNotSatisfied()
+    {
+        // Arrange
+        var logger = Mogger<FakeService>.Create(_testOutputHelper);
+        var service = new FakeService(logger);
+
+        // Act
+        service.DoWork();
+
+        // Assert
+        logger.MustNotHaveLogged(args =>
+        {
+            var message = args.Get<object>("state");
+            return message?.ToString() == "Definitely not the same message";
+        });
+    }
+
+    [Theory]
+    [InlineData(LogLevel.Trace)]
+    [InlineData(LogLevel.Debug)]
+    [InlineData(LogLevel.Warning)]
+    [InlineData(LogLevel.Error)]
+    [InlineData(LogLevel.Critical)]
+    public void OnLogging_WithService_NotAtInfo_Log_IsNotCalled(LogLevel level)
+    {
+        // Arrange
+        var logger = Mogger<FakeService>.Create(_testOutputHelper);
+        var service = new FakeService(logger);
+
+        // Act
+        service.DoWork();
+
+        // Assert
+        logger.MustNotHaveLogged(level);
+    }
+
+    [Fact]
+    public void OnNotLogging_WithService_Log_IsNotCalled()
+    {
+        // Arrange
+        var logger = Mogger<FakeService>.Create(_testOutputHelper);
+        var service = new FakeService(logger);
+
+        // Act
+        service.DoNothing();
+
+        // Assert
+        logger.MustNotHaveLogged();
+    }
+}
---
+++ Mogger.Tests/Service/FakeService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mogger.Tests.Service;
+
+internal class FakeService
+{
+    private readonly ILogger<FakeService> _logger;
+
+    public FakeService(ILogger<FakeService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void DoWork()
+    {
+        _logger.LogInformation("Doing some work...");
+    }
+
+    public void DoNothing()
+    {
+    }
+}
